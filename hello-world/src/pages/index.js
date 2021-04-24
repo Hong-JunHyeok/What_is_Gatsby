ae9cb1b38d7fc0c5ad22910604e668162a3c97aa
HongJunHyeok <edb1631@naver.com>
2021-04-24 23:51:29 +0900

    실행

---
+++ hello-world/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <div>Hot Reload!</div>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
+}
