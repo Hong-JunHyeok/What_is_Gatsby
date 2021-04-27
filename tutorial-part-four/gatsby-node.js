79ecc72eb0d7e412f64524e5bdc3464ed1668ffa
HongJunHyeok <edb1631@naver.com>
2021-04-27 15:23:01 +0900

    파일 이름 가지고오기

--- tutorial-part-four/gatsby-node.js
+++ tutorial-part-four/gatsby-node.js
@@ -1,5 +1,6 @@
-exports.onCreateNode = ({ node }) => {
-    if (node.internal.type === `MarkdownRemark`) {
-      console.log(node.internal.type)
-    }
+exports.onCreateNode = ({ node, getNode }) => {
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent)
+    console.log(`\n`, fileNode.relativePath)
   }
+}
